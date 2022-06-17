479d30748b50d0bc3bd79862cdf77a1b6ab6a527
SurkovKostiantyn <kskrua@gmail.com>
2022-06-18 02:15:37 +0300

    Доделал!

---
+++ js/common.js
@@ -0,0 +1,13 @@
+const header = document.querySelector('#header');
+const nav = document.querySelector('#navbar');
+const callback = ([entry]) => {
+    const { isIntersecting } = entry;
+    nav.classList.toggle('navOpacity', !isIntersecting);
+};
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.0
+};
+const observer = new IntersectionObserver(callback, options);
+observer.observe(header);
